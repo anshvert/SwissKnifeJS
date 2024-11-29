6c538457901e9b0cddb405798908009da4d0ea8a
ansh <anshtyagi2222@gmail.com>
2024-11-30 02:14:50 +0530

    missed some files lol

---
+++ src/path/dir-utils/types.ts
@@ -0,0 +1,7 @@
+export type MockFileGenerateOptions = {
+    count: number,
+    extensions: string[],
+    minSizeKB: number,
+    maxSizeKB: number,
+    filePrefix?: string,
+}
---
+++ src/path/index.ts
@@ -0,0 +1,1 @@
+export * from "./dir-utils/directory.manipulator"
